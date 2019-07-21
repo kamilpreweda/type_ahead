85965dcd1efffde318373ebe9b4e42b15ca0e675
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-21 20:09:50 +0200

    added findMatches function

--- src/js/main.js
+++ src/js/main.js
@@ -23,3 +23,11 @@ const cities = [];
 fetch(endpoint) // fetch returns a promise
   .then(blob => blob.json()) // then we can modify our "blob" of data to json and get another promise
   .then(data => cities.push(...data)) // then we can push data into our array but we need to use the spread operator (...)
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // here we need to figure out if city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi'); // g - stands for global (entire array will be searched through) and i - stands for insensitive (lowercase and uppercase will be matched)
+    return place.city.match(regex) || place.state.match(regex);
+  })
+}
