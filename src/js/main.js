a30349e6ebe56f203e5637bc17f1c2072ef85af3
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-21 20:03:23 +0200

    added fetch

--- src/js/main.js
+++ src/js/main.js
@@ -18,4 +18,8 @@ if ('serviceWorker' in navigator) {
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const cities = [];
 
+fetch(endpoint) // fetch returns a promise
+  .then(blob => blob.json()) // then we can modify our "blob" of data to json and get another promise
+  .then(data => cities.push(...data)) // then we can push data into our array but we need to use the spread operator (...)
